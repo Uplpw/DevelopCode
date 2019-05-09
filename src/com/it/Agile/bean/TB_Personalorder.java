976920c5a0d2261b9e4ce4d58fdaada0162a1651
llsir <49268354+llsir@users.noreply.github.com>
2019-05-09 09:21:09 +0800

    个人中心—账户管理

---
+++ src/com/it/Agile/bean/TB_Personalorder.java
@@ -0,0 +1,45 @@
+package com.it.Agile.bean;
+
+import java.sql.*;
+
+public class TB_Personalorder {
+	private String id;
+	private Date ordertime;
+	private String username;
+	private String amount;
+	private String state;
+	
+	public TB_Personalorder() {		
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public Date getOrdertime() {
+		return ordertime;
+	}
+	public void setOrdertime(Date ordertime) {
+		this.ordertime = ordertime;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getAmount() {
+		return amount;
+	}
+	public void setAmount(String amount) {
+		this.amount = amount;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+}
