9ec9e476c71b3e351b493bd08f0e295ed6430219
llsir <49268354+llsir@users.noreply.github.com>
2019-05-09 09:21:46 +0800

    个人中心—账户管理

---
+++ src/com/it/Agile/bean/TB_Userinfo.java
@@ -0,0 +1,48 @@
+package com.it.Agile.bean;
+
+public class TB_Userinfo {
+	private String username;
+	private String petname;
+	private String userpicture;
+	private String usertelephone;
+	private String userreal;
+	private Long usercredit;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPetname() {
+		return petname;
+	}
+	public void setPetname(String petname) {
+		this.petname = petname;
+	}
+	public String getUserpicture() {
+		return userpicture;
+	}
+	public void setUserpicture(String userpicture) {
+		this.userpicture = userpicture;
+	}
+	public String getUsertelephone() {
+		return usertelephone;
+	}
+	public void setUsertelephone(String usertelephone) {
+		this.usertelephone = usertelephone;
+	}
+	public String getUserreal() {
+		return userreal;
+	}
+	public void setUserreal(String userreal) {
+		this.userreal = userreal;
+	}
+	public Long getUsercredit() {
+		return usercredit;
+	}
+	public void setUsercredit(Long usercredit) {
+		this.usercredit = usercredit;
+	}
+	
+
+}
